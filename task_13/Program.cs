3d182e482e5dae66b19cbf063f1542372cdcc277
OlikNN <oliknn@gmail.com>
2022-12-01 01:24:00 +0300

    init

---
+++ task_13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99)
+{
+    while (number >= 100)
+    {
+        Console.WriteLine($"  {number}");
+        if (number <= 999) Console.WriteLine($" третий знак {number % 10}");
+        number /= 10;
+        int d = number % 10;
+    }
+}
+else
+{
+    Console.WriteLine($"Третьей цифра нет");
+}
+
+
---
+++ task_15/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите ваше число от 1 до 7:");
+string? answer = Console.ReadLine();
+int number = Convert.ToInt32(answer);
+
+if (number > 7 || number < 1)
+{
+    Console.WriteLine("Число должно быть от 1 до 7");
+}
+
+if (number < 6)
+{
+    Console.WriteLine("Нет");
+} 
+else
+{
+    Console.WriteLine("Да");
+}
